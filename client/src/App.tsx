ad378a4e4b7496de7d67d2d5735886a809b52b25
Xasanboy <hasanbojabdurasulov309@gmail.com>
2023-02-07 21:46:25 +0500

    Auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
-import { Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
-import './App.css'
-import Header from './pages/Header'
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import "./App.css";
+import Header from "./pages/Header";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Header />} />
-        <Route path='/login' element={<Login />} />
+        <Route path="/" element={<Header />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,12 +1,15 @@
 export default function Login() {
-    return (
-        <div>
-            <form>
-                <div>
-                    <h4>Your name</h4>
-                    <input type={"text"} placeholder="John" />
-                </div>
-            </form>
+  return (
+    <div>
+      <form>
+        <div className="w-25 h-25">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsTlLIXK2P0Dv7Rvh5_8WH7M_xcqWSj6mErA&usqp=CAU" />
         </div>
-    )
+        <div>
+          <h4>Your name</h4>
+          <input type={"text"} placeholder="John" />
+        </div>
+      </form>
+    </div>
+  );
 }
